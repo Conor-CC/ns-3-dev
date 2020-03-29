89f3427b464d6459d664c5dc6bd5568bdb3204da
ConorClery <cleryc@tcd.ie>
2020-03-29 21:02:38 +0100

    Shell scripts for running generated tests in test-scripts/

---
+++ test-scripts/run-freq_0_1.sh
@@ -0,0 +1,6 @@
+bash test-scripts/freq_0_1/run-ndn-sparse-freq_0_1.sh
+bash test-scripts/freq_0_1/run-ndn-mod-freq_0_1.sh
+bash test-scripts/freq_0_1/run-ndn-dense-freq_0_1.sh
+bash test-scripts/freq_0_1/run-pcd-sparse-freq_0_1.sh
+bash test-scripts/freq_0_1/run-pcd-mod-freq_0_1.sh
+bash test-scripts/freq_0_1/run-pcd-dense-freq_0_1.sh
---
+++ test-scripts/run-freq_0_25.sh
@@ -0,0 +1,6 @@
+bash test-scripts/freq_0_25/run-ndn-sparse-freq_0_25.sh
+bash test-scripts/freq_0_25/run-ndn-mod-freq_0_25.sh
+bash test-scripts/freq_0_25/run-ndn-dense-freq_0_25.sh
+bash test-scripts/freq_0_25/run-pcd-sparse-freq_0_25.sh
+bash test-scripts/freq_0_25/run-pcd-mod-freq_0_25.sh
+bash test-scripts/freq_0_25/run-pcd-dense-freq_0_25.sh
