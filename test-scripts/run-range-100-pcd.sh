92e748fb4e6b2aa3c6b416fc83a03ed977d246c1
ConorClery <cleryc@tcd.ie>
2020-04-30 14:15:21 +0100

    removed unecessary comments from test scripts

--- test-scripts/run-range-100-pcd.sh
+++ test-scripts/run-range-100-pcd.sh
@@ -1,3 +1,3 @@
-# bash test-scripts/range-100/run-pcd-sparse-range-100.sh
-# bash test-scripts/range-100/run-pcd-mod-range-100.sh
+bash test-scripts/range-100/run-pcd-sparse-range-100.sh
+bash test-scripts/range-100/run-pcd-mod-range-100.sh
 bash test-scripts/range-100/run-pcd-dense-range-100.sh
--- test-scripts/run-range-100.sh
+++ test-scripts/run-range-100.sh
@@ -1,3 +1,3 @@
-# bash test-scripts/range-100/run-ndn-sparse-range-100.sh
-# bash test-scripts/range-100/run-ndn-mod-range-100.sh
+bash test-scripts/range-100/run-ndn-sparse-range-100.sh
+bash test-scripts/range-100/run-ndn-mod-range-100.sh
 bash test-scripts/range-100/run-ndn-dense-range-100.sh
